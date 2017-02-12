e1d29ab24264f1e0ce99deb690db97a78b08d773
Gates Zeng <gatesazeng@gmail.com>
2017-02-12 13:51:51 -0800

    added low-res to high-res image loading

--- flicks/DetailViewController.swift
+++ flicks/DetailViewController.swift
@@ -36,9 +36,62 @@ class DetailViewController: UIViewController {
             posterImageView.setImageWith(imageURL as! URL)
         }
         
+        let smallBaseURL = "https://image.tmdb.org/t/p/w45"
+        let largeBaseURL = "https://image.tmdb.org/t/p/original"
+        if let posterPath = movie["poster_path"] as? String {
+            let smallImageURL = URL(string: smallBaseURL + posterPath)
+            let largeImageURL = URL(string: largeBaseURL + posterPath)
+            
+            let smallImageRequest = NSURLRequest(url: smallImageURL!)
+            let largeImageRequest = NSURLRequest(url: largeImageURL!)
+            
+            self.posterImageView.setImageWith(
+                smallImageRequest as URLRequest,
+                placeholderImage: nil,
+                success: { (smallImageRequest, smallImageResponse, smallImage) -> Void in
+                    
+                    // smallImageResponse will be nil if the smallImage is already available
+                    // in cache (might want to do something smarter in that case).
+                    self.posterImageView.alpha = 0.0
+                    self.posterImageView.image = smallImage;
+                    
+                    self.setLargeImage(largeImageRequest: largeImageRequest, placeholder: smallImage)
+                    
+                },
+                failure: { (request, response, error) -> Void in
+                    // do something for the failure condition
+                    // possibly try to get the large image
+                    self.setLargeImage(largeImageRequest: largeImageRequest, placeholder: #imageLiteral(resourceName: "poster_placeholder"))
+            })
+        }
         print(movie)
+    }
+    
+    func setLargeImage(largeImageRequest: NSURLRequest, placeholder: UIImage) {
+        UIView.animate(withDuration: 0.3, animations: { () -> Void in
+            
+            self.posterImageView.alpha = 1.0
+            
+            }, completion: { (sucess) -> Void in
+                
+                // The AFNetworking ImageView Category only allows one request to be sent at a time
+                // per ImageView. This code must be in the completion block.
+                self.posterImageView.setImageWith(
+                    largeImageRequest as URLRequest,
+                    placeholderImage: placeholder,
+                    success: { (largeImageRequest, largeImageResponse, largeImage) -> Void in
+                        
+                        self.posterImageView.image = largeImage;
+                        
+                    },
+                    failure: { (request, response, error) -> Void in
+                        // do something for the failure condition of the large image request
+                        // possibly setting the ImageView's image to a default image
+                        self.posterImageView.image = #imageLiteral(resourceName: "poster_placeholder");
+                        
+                })
+        })
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
